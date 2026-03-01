cac75225846bf90e17702188f67745044358c060
bazwahh-cloud <bazwahh@gmail.com>
2026-03-01 10:25:59 +0000

    Update index.php

--- wp-content/themes/dance-offs-shop-theme/index.php
+++ wp-content/themes/dance-offs-shop-theme/index.php
@@ -1,1 +1,9 @@
+<?php
+
+// Enable WooCommerce support
+add_action('after_setup_theme', function() {
+    add_theme_support('woocommerce');
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+});
 
